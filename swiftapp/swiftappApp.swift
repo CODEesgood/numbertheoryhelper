e7c8724c18b5f5483ee5d9f4b10297359d831a3c
CODEesgood <94589804+CODEesgood@users.noreply.github.com>
2023-10-09 23:41:45 -0700

    Initial Commit

---
+++ swiftapp/swiftappApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftappApp.swift
+//  swiftapp
+//
+//  Created by Jiaheng Li  on 10/9/23.
+//
+
+import SwiftUI
+
+@main
+struct swiftappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
